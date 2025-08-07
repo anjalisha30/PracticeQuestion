4235998520d5a1a1ccd35c3028dd4db7ad0f71f1
anjalisha30 <anjalishakya0530@gmail.com>
2025-08-07 14:56:42 +0530

    Solved Leetcode Problem 2235 in Java

---
+++ Java/Leetcode/LC2235.java
@@ -0,0 +1,12 @@
+package Java.Leetcode;
+
+
+public class LC2235 {
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum: " + sum(10, 25));
+    }
+}
--- Java/Leetcode/LC3162.java
+++ Java/Leetcode/LC3162.java
@@ -1,31 +1,27 @@
 package Java.Leetcode;
-class Solution {
-    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+
+class LC3162{
+    public static int numberOfPairs(int[] nums1, int[] nums2, int k) {
         int n1 = nums1.length;
         int n2 = nums2.length;
         int count = 0;
         for(int i = 0; i<n1; i++){
             for(int j = 0; j<n2; j++){
                 if(nums1[i] % (nums2[j] *k) == 0){
-                count++;
+                    count++;
                 }
             }
         }
         return count;
     }
-
     
-}
-
-class LC3162{
      public static void main(String[] args) {
-        Solution obj = new Solution();
 
         int[] nums1 = {1, 3, 4}; // example nums1
         int[] nums2 = {1, 3, 4};           // example nums2
         int k = 1;
 
-        int result = obj.numberOfPairs(nums1, nums2, k);
+        int result = numberOfPairs(nums1, nums2, k);
 
         System.out.println("Number of valid pairs: " + result);
     }
